bc57c2989b80a45408c0550c85a8941b40693bdd
Maitiu O Ciarain <m.ociarain@gmail.com>
2019-01-17 17:14:54 +0000

    Remove the cli client for the new faculty projects client

---
+++ test/test_projects.py
@@ -0,0 +1,58 @@
+import pytest
+from click.testing import CliRunner
+
+from faculty_cli.cli.cli import cli
+from faculty.clients.project import ProjectClient
+from test.fixtures import PROJECT, USER_ID
+
+
+@pytest.fixture
+def mock_update_check(mocker):
+    mocker.patch("faculty.cli.update.check_for_new_release")
+
+
+@pytest.fixture
+def mock_check_credentials(mocker):
+    mocker.patch("faculty.cli.cli._check_credentials")
+
+
+@pytest.fixture
+def mock_profile(mocker):
+    mocker.patch("faculty.cli.auth.user_id", return_value=USER_ID)
+
+
+def test_list_projects(
+    mocker, mock_update_check, mock_check_credentials, mock_profile
+):
+    runner = CliRunner()
+    schema_mock = mocker.patch("faculty.clients.project.ProjectSchema")
+    mocker.patch.object(ProjectClient, "_get", return_value=[PROJECT])
+
+    result = runner.invoke(cli, ["projects"])
+
+    assert result.exit_code == 0
+    assert result.output == f"{PROJECT.name}\n"
+
+    ProjectClient._get.assert_called_once_with(
+        "/user/{}".format(USER_ID), schema_mock.return_value
+    )
+
+
+def test_list_projects_verbose(
+    mocker, mock_update_check, mock_check_credentials, mock_profile
+):
+    runner = CliRunner()
+    schema_mock = mocker.patch("faculty.clients.project.ProjectSchema")
+    mocker.patch.object(ProjectClient, "_get", return_value=[PROJECT])
+
+    result = runner.invoke(cli, ["projects", "-v"])
+
+    assert result.exit_code == 0
+    assert (
+        result.output
+        == f"Project Name    ID\n{PROJECT.name}    {PROJECT.id}\n"
+    )
+
+    ProjectClient._get.assert_called_once_with(
+        "/user/{}".format(USER_ID), schema_mock.return_value
+    )
