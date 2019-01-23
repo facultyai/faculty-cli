d7be4684395401815f2dd1568b0d48df87436049
Maitiu O Ciarain <m.ociarain@gmail.com>
2019-01-23 13:15:58 +0000

    Remove unnecessesary binding casuing flake8 warning

--- test/conftest.py
+++ test/conftest.py
@@ -5,6 +5,4 @@ from test.fixtures import PROFILE
 
 @pytest.fixture
 def mock_profile(mocker):
-    profile_mock = mocker.patch(
-        "faculty.config.resolve_profile", return_value=PROFILE
-    )
+    mocker.patch("faculty.config.resolve_profile", return_value=PROFILE)
