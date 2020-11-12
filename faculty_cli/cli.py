ec2b68637ff0016241d37da91c75effe748e2083
Andrew Crozier <wacrozier@gmail.com>
2020-11-12 17:26:33 +0000

    Fix license headers

--- faculty_cli/cli.py
+++ faculty_cli/cli.py
@@ -1,5 +1,3 @@
-"""Command line interface."""
-
 # Copyright 2016-2020 Faculty Science Limited
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -13,6 +11,8 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+"""Command line interface."""
 
 from __future__ import division
 
